902ab5e98309d3841f605582f77b45a330c8fc85
luizaelenaa <luiza_elena_a@yahoo.com>
2020-04-03 19:20:55 +0300

    lab5

---
+++ lab5/src/whyuseinterfaces/example3/Dog.java
@@ -0,0 +1,8 @@
+
+public class Dog extends WashableObject {
+
+    @Override
+    public void wash() {
+        System.out.println("washing a dog");
+    }
+}
---
+++ lab5/src/whyuseinterfaces/model/animal/Animal.java
@@ -0,0 +1,14 @@
+package whyuseinterfaces.model.animal;
+
+/**
+ * <p>
+ * this class doesn't implement washable because we don't want all subclasses to be forced to describe
+ * a behavior they don't necessarily have
+ */
+public abstract class Animal {
+
+    private String species;
+    private int age;
+
+    public abstract void eat();
+}
